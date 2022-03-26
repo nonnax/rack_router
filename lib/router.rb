1bb04ac852fdf44735184c6fa61eee488cbfae2c
nonnax <ironald@gmail.com>
2022-03-26 23:00:48 +0800

    changed routing lookup technique

--- lib/router.rb
+++ lib/router.rb
@@ -18,17 +18,17 @@ class String
 end
 
 class Router
+  
   @routes = Hash.new([])
-  def self.routes
-    @routes
-  end
-
-  def self.status
-     @status ||= 200
-  end
-
-  def self.status=(code)
-     @status=code
+  @apps = []
+  
+  class << self
+    def apps() = @apps
+    def routes() = @routes
+    def status() = @status||=200
+    def status=(code)
+      @status=code
+    end
   end
   
   def initialize
@@ -44,32 +44,31 @@ class Router
     @env = env
     @req = Rack::Request.new @env
     res = Rack::Response.new
-    self.class.status=res.status
+    self.class.status = res.status
     
-    controller, _ = self.class.routes[@req.path_info]
+    controller,_ = self.class.apps.detect{|c, _| c.path_pattern.match @req.path_info }
     
     return controller.call(env) if controller   
-    (self.class.status=404 ) && NotFound.call(env)
-    
+    (self.class.status = 404 ) && NotFound.call(env)   
   end
 
-  def call(env)
-    dup._call(env)
-  end
+  def call(env) = dup._call(env)
 end
 
 def R(url)
     # controller class and route
     Class.new {
       def self.call(env)
-        m=env['REQUEST_METHOD'].downcase.to_sym
-        @body=self.send(m, env)
+        m = env['REQUEST_METHOD'].downcase.to_sym
+        @body = self.send(m, env)
         [Router.status, {'Content-Type' => 'text/html'}, ["#{@body}"]]
       end
-      meta_def(:path_pattern){ /^#{url}$/ }
+      meta_def(:path_pattern){/^#{url}$/}
+      meta_def(:path){ url }
       meta_def(:inherited){|x| 
-        norm_name=x.to_s.snakecase
+        norm_name = x.to_s.snakecase
         Router.routes[url] = [x, File.join(APPS_PATH, norm_name)] 
+        Router.apps << [x, File.join(APPS_PATH, norm_name)] 
       }
     }
 end
